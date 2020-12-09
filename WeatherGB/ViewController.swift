41e179760bf1656de41ddb12de58abb4a5f0797c
evgenyderbenev <evg6041@ya.ru>
2020-12-09 22:16:04 +0300

    v1

---
+++ WeatherGB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGB
+//
+//  Created by Евгений Дербенев on 30.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+    
