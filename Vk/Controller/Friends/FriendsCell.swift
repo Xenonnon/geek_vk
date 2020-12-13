5901fc63aec58ecbe26a39772b5e34570bec4b0f
evgenyderbenev <evg6041@ya.ru>
2020-12-14 01:20:16 +0300

    v3

---
+++ Vk/Controller/Friends/FriendsCell.swift
@@ -0,0 +1,16 @@
+//
+//  FriendsCell.swift
+//  Vk
+//
+//  Created by Евгений Дербенев on 13.12.2020.
+//
+
+import UIKit
+
+class FriendsCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var friendPhoto: UIImageView!
+    @IBOutlet weak var friendName: UILabel!
+
+}
---
+++ Vk/Controller/Photo/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  Vk
+//
+//  Created by Евгений Дербенев on 13.12.2020.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var friendName: UILabel!
+    @IBOutlet weak var photo: UIImageView!
+    
+}
